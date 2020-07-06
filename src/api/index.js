a8bd240084add7f48e36d8aee96eba51a45cbbd1
sulihua <l1156832448@qq.com>
2020-07-06 21:44:59 +0800

    内容管理-axios请求拦截器

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,25 @@
 // 导出配置好的axios提供给main.js去使用
 import axios from 'axios'
+import auth from '../utils/auth'
 
 // 进行axios的配置，将来这回又很多axios相关的配置
 axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0/'
+
+// 请求头  配置token
+axios.defaults.headers.Authorization = `Bearer ${auth.getUser().token}`
+
+// 添加请求拦截器
+axios.interceptors.request.use(function (config) {
+  // 在发送请求之前做些什么
+  const { token } = auth.getUser()
+  // 做一个严谨的判断，本地存储了token就是追加
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config
+}, function (error) {
+// 对请求错误做些什么
+  return Promise.reject(error)
+})
 
 export default axios
--- src/views/Article.vue
+++ src/views/Article.vue
@@ -6,8 +6,14 @@
 
 <script>
 export default {
-  name: 'my-article'
+  name: 'my-article',
+  created () {
+    this.$http.get('articles').then((res) => {
+      console.log(res)
+    })
+  }
 }
 </script>
 
 <style scoped lang='less'></style>
+
