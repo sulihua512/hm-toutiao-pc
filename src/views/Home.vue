5d9591817c7be5216e8d2c8dc23e31707b7c7149
sulihua <l1156832448@qq.com>
2020-07-06 14:16:23 +0800

    首页模块-头部内容

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,7 +6,25 @@
     <!-- 右侧主体 -->
     <el-container>
       <!-- 头部内容 -->
-      <el-header class="my-header">Header</el-header>
+      <el-header class="my-header">
+          <!-- 图标 -->
+        <span class="el-icon-s-fold icon"></span>
+        <!-- 文字 -->
+        <span class="text">江苏传智播客科技教育有限公司</span>
+      <el-dropdown class="my-dropdown">
+        <span class="el-dropdown-link">
+            <!-- 头像 -->
+            <img class="user-avatar" src="../assets/avatar.jpg">
+            <!-- 名字 -->
+            <span class="user-name">张三丰</span>
+            <i class="el-icon-arrow-down el-icon--right"></i>
+          </span>
+            <el-dropdown-menu slot="dropdown">
+                <el-dropdown-item icon="el-icon-setting">个人设置</el-dropdown-item>
+                <el-dropdown-item icon="el-icon-unlock">退出登录</el-dropdown-item>
+            </el-dropdown-menu>
+        </el-dropdown>
+      </el-header>
       <!-- 主体内容 -->
       <el-main>Main</el-main>
     </el-container>
@@ -36,6 +54,28 @@ export default {
     }
     .my-header{
         border-bottom: 1px solid #ddd;
+        line-height: 60px;
+        .icon {
+            font-size: 24px;
+            vertical-align: middle;
+        }
+        .text {
+            vertical-align: middle;
+            padding-left: 10px;
+        }
+    }
+    .my-dropdown {
+        float: right;
+        .user-avatar {
+            width: 30px;
+            height: 30px;
+            vertical-align: middle;
+        }
+        .user-name {
+            font-weight: bold;
+            vertical-align: middle;
+            padding-left: 5px;
+        }
     }
 }
 </style>
