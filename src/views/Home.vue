b415fb5c8df3cf7c844f614a05e4227554dd54a3
sulihua <l1156832448@qq.com>
2020-07-06 14:39:31 +0800

    首页模块-侧边栏

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,46 @@
  <!-- 全屏容器 -->
   <el-container class="home-container">
     <!-- 侧边栏 -->
-    <el-aside class="my-aside" width="200px">Aside</el-aside>
+    <el-aside class="my-aside" width="200px">
+        <!-- logo -->
+        <div class="logo"></div>
+        <!-- 导航菜单 -->
+        <el-menu
+      style="border-right:none"
+      default-active="1"
+      background-color="#002233"
+      text-color="#fff"
+      active-text-color="#ffd04b">
+        <el-menu-item index="1">
+          <i class="el-icon-s-home"></i>
+          <span slot="title">首页</span>
+        </el-menu-item>
+        <el-menu-item index="2">
+          <i class="el-icon-document"></i>
+          <span slot="title">内容管理</span>
+        </el-menu-item>
+        <el-menu-item index="3">
+          <i class="el-icon-picture"></i>
+          <span slot="title">素材管理</span>
+        </el-menu-item>
+        <el-menu-item index="4">
+          <i class="el-icon-s-promotion"></i>
+          <span slot="title">发布文章</span>
+        </el-menu-item>
+        <el-menu-item index="5">
+          <i class="el-icon-chat-dot-round"></i>
+          <span slot="title">评论管理</span>
+        </el-menu-item>
+        <el-menu-item index="6">
+          <i class="el-icon-present"></i>
+          <span slot="title">粉丝管理</span>
+        </el-menu-item>
+        <el-menu-item index="7">
+          <i class="el-icon-setting"></i>
+          <span slot="title">个人设置</span>
+        </el-menu-item>
+      </el-menu>
+    </el-aside>
     <!-- 右侧主体 -->
     <el-container>
       <!-- 头部内容 -->
@@ -11,14 +50,14 @@
         <span class="el-icon-s-fold icon"></span>
         <!-- 文字 -->
         <span class="text">江苏传智播客科技教育有限公司</span>
-      <el-dropdown class="my-dropdown">
-        <span class="el-dropdown-link">
-            <!-- 头像 -->
-            <img class="user-avatar" src="../assets/avatar.jpg">
-            <!-- 名字 -->
-            <span class="user-name">张三丰</span>
-            <i class="el-icon-arrow-down el-icon--right"></i>
-          </span>
+        <el-dropdown class="my-dropdown">
+            <span class="el-dropdown-link">
+                <!-- 头像 -->
+                <img class="user-avatar" src="../assets/avatar.jpg">
+                <!-- 名字 -->
+                <span class="user-name">张三丰</span>
+                <i class="el-icon-arrow-down el-icon--right"></i>
+            </span>
             <el-dropdown-menu slot="dropdown">
                 <el-dropdown-item icon="el-icon-setting">个人设置</el-dropdown-item>
                 <el-dropdown-item icon="el-icon-unlock">退出登录</el-dropdown-item>
@@ -75,6 +114,14 @@ export default {
             font-weight: bold;
             vertical-align: middle;
             padding-left: 5px;
+        }
+    }
+    .my-aside {
+        background: #002233;
+        .logo {
+            width: 100%;
+            height: 60px;
+            background: #002244 url(../assets/logo_admin.png) no-repeat center /140px auto;
         }
     }
 }
