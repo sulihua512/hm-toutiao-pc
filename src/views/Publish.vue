0136ba6b8d6356efc257d490fed08b2e12eff223
sulihua <l1156832448@qq.com>
2020-07-09 17:11:41 +0800

    发布文章-富文本使用

--- src/views/Publish.vue
+++ src/views/Publish.vue
@@ -10,7 +10,7 @@
           <el-input v-model="articleForm.title" placeholder="请输入文章标题" style="width:400px"></el-input>
         </el-form-item>
         <el-form-item label="内容:">
-          富文本
+         <quill-editor v-model="articleForm.content" :options="editorOption"></quill-editor>
         </el-form-item>
         <el-form-item label="封面:">
           <el-radio-group v-model="articleForm.cover.type">
@@ -22,7 +22,7 @@
           <div>封面组件</div>
         </el-form-item>
         <el-form-item label="频道:">
-          频道组件
+          <my-channel v-model="articleForm.channel_id"></my-channel>
         </el-form-item>
         <el-form-item>
           <el-button type="primary">发布文章</el-button>
@@ -34,8 +34,17 @@
 </template>
 
 <script>
+// 导入样式
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+// 导入组件
+import { quillEditor } from 'vue-quill-editor'
 export default {
   name: 'my-publish',
+  components: {
+    quillEditor
+  },
   data() {
     return {
        // 文章表单数据对象
@@ -47,7 +56,9 @@ export default {
           images: []
         },
         channel_id: null
-      }
+      },
+        // 富文本配置对象
+      editorOption: {}
     }
   }
 }
