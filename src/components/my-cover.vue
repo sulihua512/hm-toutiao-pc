d61b076c6db2bd9d6f31d7c4423058684c276add
sulihua <l1156832448@qq.com>
2020-07-09 17:35:30 +0800

    发布文章-封面组件-素材库列表

--- src/components/my-cover.vue
+++ src/components/my-cover.vue
@@ -9,21 +9,26 @@
         <el-tabs v-model="activeName" type="card" >
             <el-tab-pane label="素材库" name="image">
                  <!-- 切换按钮 -->
-          <el-radio-group v-model="reqParams.collect" size="small">
+          <el-radio-group @change="changeCollect" v-model="reqParams.collect" size="small">
             <el-radio-button :label="false">全部</el-radio-button>
             <el-radio-button :label="true">收藏</el-radio-button>
           </el-radio-group>
           <!-- 素材列表 -->
           <div class="img_list">
-            <div class="img_item" v-for="i in 8" :key="i">
-              <img src="../assets/avatar.jpg">
+             <div class="img_item" v-for="item in images" :key="item.id">
+                <img :src="item.url">
             </div>
           </div>
           <!-- 分页组件 -->
+         <!-- 分页组件 -->
           <el-pagination
             background
             layout="prev, pager, next"
-            :total="1000">
+            hide-on-single-page
+            @current-change="changePager"
+            :page-size="reqParams.per_page"
+            :current-page="reqParams.page"
+            :total="total">
           </el-pagination>
             </el-tab-pane>
         <el-tab-pane label="上传图片" name="upload">上传图片内容</el-tab-pane>
@@ -51,12 +56,33 @@ export default {
             // 控制对话框显示隐藏
             dialogVisible: false,
             // 当前tabs组件激活的选项卡的name属性的值
-            activeName: 'image'
+            activeName: 'image',
+            // 素材列表
+            images: [],
+            // 总条数
+            total: 0
         }
     },
     methods:{
         openDialog(){
             this.dialogVisible = true;
+            this.getImages()
+        },
+        // 全部 收藏
+        changeCollect () {
+            this.reqParams.page = 1
+            this.getImages()
+        },
+        // 分页函数
+        changePager (newPage) {
+            this.reqParams.page = newPage
+            this.getImages()
+        },
+        // 获取素材列表
+        async getImages () {
+            const { data: { data } } = await this.$http.get('user/images', { params: this.reqParams })
+            this.images = data.results
+            this.total = data.total_count
         }
     }
 }
