cf4c5fa7c915d6ecb17bf98a8b690fc540c79ef2
sulihua <l1156832448@qq.com>
2020-07-09 19:27:22 +0800

    评论管理-列表与分页

--- src/views/Comment.vue
+++ src/views/Comment.vue
@@ -1,18 +1,33 @@
 <template>
   <div class='comment-container'>
-      <div slot="header">
+      <el-card>
+          <div slot="header">
           <my-bread>评论管理</my-bread>
       </div>
       <!-- 表格 -->
       <el-table :data="comments">
-          <el-table-column label="文章标题"></el-table-column>
-        <el-table-column label="总评论数"></el-table-column>
-        <el-table-column label="粉丝评论数"></el-table-column>
-        <el-table-column label="评论状态"></el-table-column>
-        <el-table-column label="操作"></el-table-column>
+          <el-table-column label="文章标题" prop="title"></el-table-column>
+        <el-table-column label="总评论数" prop="total_comment_count"></el-table-column>
+        <el-table-column label="粉丝评论数" prop="fans_comment_count"></el-table-column>
+        <el-table-column label="评论状态">
+            <template slot-scope="scope">
+                {{scope.row.comment_status?'关闭':'正常'}}
+            </template>
+        </el-table-column>
+        <el-table-column label="操作">
+            <template slot-scope="scope">
+                <el-button size="small" type="success" v-if="scope.row.comment_status">打开评论</el-button>
+                <el-button size="small" type="danger" v-else>关闭评论</el-button>
+            </template>
+        </el-table-column>
       </el-table>
       <!-- 分页 -->
-      <el-pagination style="margin-top:20px" background layout="prev, pager, next" :total="1000"></el-pagination>
+     <el-pagination style="margin-top:20px" background layout="prev, pager, next"
+      :current-page="reqParams.page"
+      :page-size="reqParams.per_page"
+      @current-change="changePager"
+       :total="total"></el-pagination>
+      </el-card>
   </div>
 </template>
 
@@ -22,8 +37,33 @@ export default {
   data(){
     return  {
         // 评论列表数据
-        comments:[]
+        comments:[],
+        // 参数对象
+        reqParams:{
+            response_type:'comment',
+            page:1,
+            per_page:20
+        },
+        // 总条数
+        total:0
     }
+  },
+  created(){
+      // 组件初始化执行函数
+      this.getComments()
+  },
+  methods:{
+      // 分页
+      changePager(newPage){
+          this.reqParams.page = newPage
+          this.getComments()
+      },
+      // 获取评论列表
+      async getComments(){
+           const { data: { data } } = await this.$http.get('articles', { params: this.reqParams })
+          this.comments = data.results
+          this.total = data.total_count
+      }
   }
 }
 </script>
