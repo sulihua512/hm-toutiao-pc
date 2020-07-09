3e6c3cc191bb6622d0c7313ad15a982db317ee52
sulihua <l1156832448@qq.com>
2020-07-09 16:56:22 +0800

    -发布文章-基础布局

--- src/views/Publish.vue
+++ src/views/Publish.vue
@@ -1,10 +1,55 @@
 <template>
-  <div class='publish-container'>Publish</div>
+  <div class='publish-container'>
+    <el-card>
+      <div slot="header">
+        <my-bread>发布文章</my-bread>
+      </div>
+      <!-- 表单 -->
+      <el-form label-width="120px">
+        <el-form-item label="标题:">
+          <el-input v-model="articleForm.title" placeholder="请输入文章标题" style="width:400px"></el-input>
+        </el-form-item>
+        <el-form-item label="内容:">
+          富文本
+        </el-form-item>
+        <el-form-item label="封面:">
+          <el-radio-group v-model="articleForm.cover.type">
+            <el-radio :label="1">单图</el-radio>
+            <el-radio :label="3">三图</el-radio>
+            <el-radio :label="0">无图</el-radio>
+            <el-radio :label="-1">自动</el-radio>
+          </el-radio-group>
+          <div>封面组件</div>
+        </el-form-item>
+        <el-form-item label="频道:">
+          频道组件
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary">发布文章</el-button>
+          <el-button>存入草稿</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'my-publish'
+  name: 'my-publish',
+  data() {
+    return {
+       // 文章表单数据对象
+      articleForm: {
+        title: null,
+        content: null,
+        cover: {
+          type: 1,
+          images: []
+        },
+        channel_id: null
+      }
+    }
+  }
 }
 </script>
 
