05ce8341b0657af9b7bf7f93f2fae7e4c2fe6d51
sulihua <l1156832448@qq.com>
2020-07-09 09:34:45 +0800

    内容管理-面包屑组件封装

--- src/views/Article.vue
+++ src/views/Article.vue
@@ -4,10 +4,7 @@
     <el-card>
       <!-- 面包屑 -->
       <div slot="header">
-        <el-breadcrumb separator-class="el-icon-arrow-right">
-          <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-          <el-breadcrumb-item>内容管理</el-breadcrumb-item>
-        </el-breadcrumb>
+        <my-bread>内容管理</my-bread>
       </div>
       <!-- 表单 -->
       <el-form label-width="80px" size="small">
@@ -50,8 +47,12 @@
 </template>
 
 <script>
+import MyBread from '../components/my-bread'
 export default {
   name: 'my-article',
+  components:{
+    MyBread
+  },
   data () {
     return {
       // 筛选条件对象数据
