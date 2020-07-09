1d4d44e979e9619ce285a073d50ab6ad8ff721bc
sulihua <l1156832448@qq.com>
2020-07-09 19:16:11 +0800

    评论管理-基础布局

--- src/views/Comment.vue
+++ src/views/Comment.vue
@@ -1,10 +1,30 @@
 <template>
-  <div class='comment-container'>Comment</div>
+  <div class='comment-container'>
+      <div slot="header">
+          <my-bread>评论管理</my-bread>
+      </div>
+      <!-- 表格 -->
+      <el-table :data="comments">
+          <el-table-column label="文章标题"></el-table-column>
+        <el-table-column label="总评论数"></el-table-column>
+        <el-table-column label="粉丝评论数"></el-table-column>
+        <el-table-column label="评论状态"></el-table-column>
+        <el-table-column label="操作"></el-table-column>
+      </el-table>
+      <!-- 分页 -->
+      <el-pagination style="margin-top:20px" background layout="prev, pager, next" :total="1000"></el-pagination>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'my-comment'
+  name: 'my-comment',
+  data(){
+    return  {
+        // 评论列表数据
+        comments:[]
+    }
+  }
 }
 </script>
 
